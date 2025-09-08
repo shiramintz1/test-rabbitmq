504b2a6508e50862d59e9a55233993901ae3906e
shiramintz1 <shiramintz2004@gmail.com>
2025-09-08 14:38:44 +0300

    add a test

--- RabbitMQ/connection.py
+++
@@ -1,6 +0,0 @@
-import pika
-
-def create_connection(host, port, username, password):
-    credentials = pika.PlainCredentials(username, password)
-    parameters = pika.ConnectionParameters(host=host, port=port, credentials=credentials)
-    return pika.BlockingConnection(parameters)
